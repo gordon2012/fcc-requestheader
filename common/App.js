f1b8686721e262aa2f9255b27a86729877878b53
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-11-22 11:07:39 -0700

    Complete front end
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- common/App.js
+++ common/App.js
@@ -1,38 +1,76 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = { responses: [] };
+
+  handleSubmit = () => {
+    fetch('api/whoami').then(response =>
+      response.json().then(data =>
+        this.setState({
+          responses: [...this.state.responses, JSON.stringify(data)]
+        })
+      )
+    );
+  };
+
+  handleClear = () => {
+    this.setState({ responses: [] });
+  };
+
   render() {
     return (
       <div className="app">
-        <h1 className="masthead">Request Header Microservice</h1>
+        <main>
+          <h1 className="masthead">Request Header Microservice</h1>
 
-        <div className="container">
-          <div className="card">
-            <h2 className="masthead">User Story</h2>
-            <ol>
-              <li>
-                I can get the IP address, preferred languages (from header
-                Accept-Language) and system infos (from header User-Agent) for
-                my device.
-              </li>
-            </ol>
-          </div>
+          <div className="container">
+            <div className="card">
+              <h2 className="masthead">User Story</h2>
+              <ol>
+                <li>
+                  I can get the IP address, preferred languages (from header
+                  Accept-Language) and system infos (from header User-Agent) for
+                  my device.
+                </li>
+              </ol>
+            </div>
 
-          <div className="card">
-            <h2 className="masthead">Request Information</h2>
-            <p>
-              <code>GET /api/whoami</code>
-            </p>
-            <div className="masthead">
-              <button>Submit</button>
+            <div className="card">
+              <h2 className="masthead">Request Information</h2>
+              <p>
+                <code>GET /api/whoami</code>
+              </p>
+              <div className="buttons">
+                <div>
+                  <button onClick={this.handleSubmit}>Submit</button>
+                </div>
+                <div>
+                  <button onClick={this.handleClear}>Clear</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="card">
+              <h2 className="masthead">Output</h2>
+              <div className="responses">
+                {this.state.responses.map((response, i) => (
+                  <code key={i}>{response}</code>
+                ))}
+              </div>
             </div>
           </div>
-
-          <div className="card">
-            <h2 className="masthead">Output</h2>
-            <div className="responses" />
-          </div>
-        </div>
+        </main>
+        <footer>
+          A{' '}
+          <a
+            href="https://freecodecamp.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            freeCodeCamp
+          </a>{' '}
+          APIs and Microservices Project
+        </footer>
       </div>
     );
   }
