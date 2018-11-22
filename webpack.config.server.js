3117719b7dd96ad8933f2ef8cd8fa92c24880f9b
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-11-21 21:43:45 -0700

    Add production build script
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -4,39 +4,49 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const StartServerPlugin = require('start-server-webpack-plugin');
 
-module.exports = {
-  entry: {
-    index: ['webpack/hot/poll?1000', './server/index']
-  },
-  watch: true,
-  target: 'node',
-  externals: [
-    nodeExternals({
-      whitelist: ['webpack/hot/poll?1000']
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  plugins: [
-    new StartServerPlugin('server.js'),
+module.exports = env => {
+  const production = !!(env && env.NODE_ENV && env.NODE_ENV === 'production');
+
+  let plugins = [
     new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       process_env: {
         BUILD_TARGET: JSON.stringify('server')
       }
     })
-  ],
-  output: {
-    path: path.join(__dirname, '.build'),
-    filename: 'server.js'
+  ];
+
+  if (!production) {
+    plugins.push(new StartServerPlugin('server.js'));
+    plugins.push(new webpack.HotModuleReplacementPlugin());
   }
+
+  return {
+    entry: {
+      index: ['webpack/hot/poll?1000', './server/index']
+    },
+    mode: production ? 'production' : 'development',
+    watch: !production,
+    target: 'node',
+    externals: [
+      nodeExternals({
+        whitelist: ['webpack/hot/poll?1000']
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins,
+    output: {
+      path: path.join(__dirname, '.build'),
+      filename: 'server.js'
+    }
+  };
 };
