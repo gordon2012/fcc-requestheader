b3b75ec78d0a1508e1180ce573a225b38945ca52
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-10-20 00:04:39 -0700

    Add now deploy
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -2,65 +2,65 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = env => {
-  const production = !!(env && env.NODE_ENV && env.NODE_ENV === 'production');
+    const production = !!(env && env.NODE_ENV && env.NODE_ENV === 'production');
 
-  let plugins = [
-    new webpack.NamedModulesPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        BUILD_TARGET: JSON.stringify('client')
-      }
-    })
-  ];
-  if (!production) {
-    plugins.push(new webpack.HotModuleReplacementPlugin());
-  }
-
-  const entry = production
-    ? ['./client/index']
-    : [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:4002',
-        'webpack/hot/only-dev-server',
-        './client/index'
-      ];
-
-  return {
-    devtool: 'inline-source-map',
-    entry,
-    mode: production ? 'production' : 'development',
-    target: 'web',
-    module: {
-      rules: [
-        {
-          test: /\.js?$/,
-          use: 'babel-loader',
-          include: [
-            path.join(__dirname, 'client'),
-            path.join(__dirname, 'common')
-          ]
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
-        }
-      ]
-    },
-    plugins,
-    devServer: {
-      host: 'localhost',
-      port: 4002,
-      historyApiFallback: true,
-      hot: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    },
-    output: {
-      path: path.join(__dirname, '.build'),
-      publicPath: 'http://localhost:4002/',
-      filename: 'client.js'
+    let plugins = [
+        new webpack.NamedModulesPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                BUILD_TARGET: JSON.stringify('client'),
+            },
+        }),
+    ];
+    if (!production) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
     }
-  };
+
+    const entry = production
+        ? ['./client/index']
+        : [
+              'react-hot-loader/patch',
+              'webpack-dev-server/client?http://localhost:4002',
+              'webpack/hot/only-dev-server',
+              './client/index',
+          ];
+
+    return {
+        devtool: 'inline-source-map',
+        entry,
+        mode: production ? 'production' : 'development',
+        target: 'web',
+        module: {
+            rules: [
+                {
+                    test: /\.js?$/,
+                    use: 'babel-loader',
+                    include: [
+                        path.join(__dirname, 'client'),
+                        path.join(__dirname, 'common'),
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+        plugins,
+        devServer: {
+            host: 'localhost',
+            port: 4002,
+            historyApiFallback: true,
+            hot: true,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+        },
+        output: {
+            path: path.join(__dirname, 'public'),
+            publicPath: 'http://localhost:4002/',
+            filename: 'client.js',
+        },
+    };
 };
